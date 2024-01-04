d6b944735678b087491ff7002b6f5bd3701a0802
Mike O'Brien <mikeob9@gmail.com>
2024-01-04 16:05:42 -0500

    more-informative errors

--- R/call_vdat.R
+++ R/call_vdat.R
@@ -1,10 +1,23 @@
 #'
 #' @export
 call_vdat <- function(what = '--help'){
-  system2(
-    command = Sys.getenv('VDAT_EXE'),
-    args = what,
-    stdout = TRUE, stderr = TRUE
-    )
 
+  vdat_loc <- Sys.getenv('VDAT_EXE')
+
+  if(vdat_loc == ''){
+    cli::cli_abort(
+      c(
+        "x" = "No VDAT.exe listed.",
+        "i" = "Run `vdat_here()` first."
+      )
+    )
+  }
+
+  shell_out <- sys::exec_internal(
+    cmd = Sys.getenv('VDAT_EXE'),
+    args = what
+  )
+
+  rawToChar(shell_out$stdout) |>
+    cat()
 }
--- R/vdat_here.R
+++ R/vdat_here.R
@@ -4,13 +4,12 @@ vdat_here <- function(here){
   Sys.setenv(VDAT_EXE = here)
 
   if(file.exists(Sys.getenv('VDAT_EXE'))){
-    cat(
-      'vdat.exe is located at',
-      Sys.getenv('VDAT_EXE')
+    cli::cli_alert_info(
+      "vdat.exe is located at {Sys.getenv('VDAT_EXE')}"
     )
   } else{
-    stop('There is no file at this location.')
+    cli::cli_abort(
+      c("x" = 'There is no file at this location.')
+    )
   }
-
-
 }
