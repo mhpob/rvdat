4991360b673d72af2f84a47d3669599027d055a1
Mike O'Brien <mikeob9@gmail.com>
2024-01-04 18:42:48 -0500

    function to show version

---
+++ R/vdat_version.R
@@ -0,0 +1,24 @@
+#'
+#' @export
+
+vdat_version <- function(){
+  vdat_loc <- rvdat:::check_vdat_location()
+
+  shell_out <- sys::exec_internal(
+    cmd = vdat_loc,
+    args = '--version',
+    error = FALSE
+  )
+
+  if(shell_out$status == 1){
+    cli::cli_abort(
+      c(
+        'x' = 'Call to VDAT failed.',
+        'i' = 'Is VDAT where you think it is?'
+      )
+    )
+  }
+
+  rawToChar(shell_out$stdout) |>
+    cat()
+}
