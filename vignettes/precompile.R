2d6c3a2e0a80ff25819bfcced7a85bfd03d6d2d2
Mike O'Brien <mikeob9@gmail.com>
2024-01-06 16:15:06 -0500

    more self notes

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,12 +1,20 @@
 # Precompiled vignettes that depend on `vdat.exe`
 
-# borrowed from https://github.com/ropensci/eia/blob/master/vignettes/precompile.R
-# and this blog post https://ropensci.org/blog/2019/12/08/precompute-vignettes/
+# borrowed from
+#   https://github.com/ropensci/eia/blob/master/vignettes/precompile.R
+# and this blog post
+#   https://ropensci.org/blog/2019/12/08/precompute-vignettes/
 
 # Changed file naming construct from .Rmd.Orig to _(.*).Rmd as pkgdown will
 #   skip compiling Rmd files starting with "_", but it stil allows me to
 #   build/knit/work locally.
 # https://github.com/r-lib/pkgdown/issues/1593
+
+# Workflow:
+#   1. Edit the _(.*).Rmd file
+#   2. Click RStudio "knit" button as needed
+#   3. When ready to add changes, run the resepective "knit()" below
+#   4. Commit/push to GitHub
 
 library(knitr)
 knit("vignettes/_how-to-use-rvdat.Rmd", "vignettes/how-to-use-rvdat.Rmd")
