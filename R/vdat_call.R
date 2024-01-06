4c2286291d8efdcc2f5ae0f76674ce05e1e3a6b0
Mike O'Brien <mikeob9@gmail.com>
2024-01-06 17:52:16 -0500

    update vdat_call docs

--- R/vdat_call.R
+++ R/vdat_call.R
@@ -1,12 +1,28 @@
 #' Interface directly with `vdat.exe`.
 #'
 #' Low-level interface to `vdat.exe`, wherein arguments are passed directly
-#'    to `vdat.exe`.
+#'    to `vdat.exe`. Arguments should be character vectors, each element being
+#'    one command.
 #'
 #' @param what arguments passed to `vdat.exe`. Defaults to `--help`.
 #' @param ... arguments passed to [`sys::exec_internal`].
 #'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Check the help documentation
+#' vdat_call()
+#'
+#' #
+#' vdat_call('--help')
+#'
+#' # Inspect a file; multiple arguments are passed in a character vector
+#' vdat_call(c('inspect', 'FILENAME'))
+#'
+#' }
+#'
 vdat_call <- function(what = "--help", ...) {
   vdat_loc <- check_vdat_location()
 
