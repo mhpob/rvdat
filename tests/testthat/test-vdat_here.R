5d266cc90156bdf9b57cd936a92bf46a8f89dc5c
Mike O'Brien <mikeob9@gmail.com>
2024-01-09 16:32:53 -0500

    vdat_here tests

---
+++ tests/testthat/test-vdat_here.R
@@ -0,0 +1,31 @@
+test_that("fails with no exe", {
+  err <- expect_error(
+    vdat_here('abc_123')
+  )
+
+  expect_equal(
+    err$message, c('x' = "There is no file at this location.")
+  )
+
+  expect_type(err, 'list')
+  expect_s3_class(err, c("rlang_error", "error", "condition"))
+
+})
+
+test_that('succeed with exe', {
+  readRenviron('~/.Renviron')
+
+  suppressMessages(
+    expect_no_error(
+      vdat_here(Sys.getenv('VDAT_EXE'))
+    )
+  )
+
+  message_out <- expect_message(
+    vdat_here(Sys.getenv('VDAT_EXE'))
+  )
+  expect_true(grepl(Sys.getenv('VDAT_EXE'), message_out$message))
+
+
+})
+#success with exe
