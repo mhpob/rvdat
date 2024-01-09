41e8bc5cc997cb48c986468da09952a735688015
Mike O'Brien <mikeob9@gmail.com>
2024-01-09 18:16:01 -0500

    nuke vdat_to_folder and turn it into a wrapper

--- R/vdat_to.R
+++ R/vdat_to.R
@@ -4,20 +4,16 @@
 #' @param outdir output directory for the created CSV files. Defaults to the
 #'  current working directory.
 #' @param time_corrected logical. Do you want to apply the default time correction?
+#' @param quiet logical, defaults to FALSE. Suppress messages?
+#' @param folder logical, defaults to FALSE. Convert VDAT to a folder of CSVs?
 #'
 #' @export
 #'
 vdat_to_csv <- function(vdata_file,
                         outdir = getwd(),
-                        time_corrected = FALSE) {
-  # fls <- list.files('c:/users/darpa2/analysis/chesapeake-backbone/embargo/raw/20230912', pattern = 'VR2AR_\\d.*vrl', full.names = T)
-  # to_csv(fls[2:3])
-  # list.files('c:/users/darpa2/analysis/chesapeake-backbone/embargo/raw/20230912', pattern = 'VR2AR_\\d.*vrl', full.names = T)
-
-  ## single Fathom CSV file
-  #   vdat convert --format=csv.fathom <input_file>
-  ## folder of split Fathom CSV files
-  # vdat convert --format=csv.fathom.split <input_file>
+                        time_corrected = FALSE,
+                        quiet = FALSE,
+                        folder = FALSE) {
 
   # Check that only 1 file has been passed
   if (length(vdata_file) > 1) {
@@ -25,9 +21,13 @@ vdat_to_csv <- function(vdata_file,
   }
 
   # Build arguments
+  format <- ifelse(folder == FALSE,
+                   '--format=csv.fathom',
+                   '--format=csv.fathom.split')
+
   vdat_args <- c(
     "convert",
-    "--format=csv.fathom",
+    format,
     vdata_file,
     paste0("--output=", outdir)
   )
@@ -40,20 +40,50 @@ vdat_to_csv <- function(vdata_file,
   }
 
   # Call vdat.exe and capture response
+  time_shell_out <- Sys.time()
   shell_out <- vdat_call(
     vdat_args
   )
 
-
   if (shell_out$status == 1) {
     error_convert(shell_out, vdata_file)
-  } else {
+  }
+
+  if(quiet == FALSE){
+    if(folder == TRUE){
+      type <- 'Files'
+      time_shell_out <- format(Sys.time(),
+                               '%Y%m%d-%H-%M-%S')
+      time_shell_out <- paste0('[', time_shell_out, '-xxxxxx]')
+
+
+      out_loc <- file.path(
+        outdir,
+        paste(
+          gsub(
+            '\\.vdat|\\.vrl',
+            paste0(time_shell_out, '.csv'),
+            basename(vdata_file)
+          ),
+          'fathom-split',
+          sep = '-'
+        )
+
+      )
+    }else{
+      type <- 'File'
+      out_loc <- file.path(
+        outdir,
+        gsub('vdat|vrl', 'csv', basename(vdata_file))
+      )
+    }
+
     cli::cli_bullets(
       c(
         "v" = "File converted:",
         " " = "{vdata_file}\n",
-        "i" = "File saved in:\n",
-        " " = "{outdir}"
+        "i" = "{type} saved in:\n",
+        " " = "{out_loc}"
       )
     )
   }
@@ -62,61 +92,13 @@ vdat_to_csv <- function(vdata_file,
 
 #' Convert a VRL file to a folder of CSVs by data type
 #'
-#' @param vdata_file path of VRL file to convert to CSV.
-#' @param outdir output directory for the created CSV files. Defaults to the
-#'    current working directory.
-#' @param time_corrected logical. Do you want to apply the default time correction?
+#' A wrapper around `vdat_to_csv` that just changes `folder = FALSE` to
+#'  `folder = TRUE`.
+#'
+#' @param ... arguments passed to `vdat_to_csv`.
 #'
 #' @export
-#' @examplesIf all(skip_on_ci(), skip_on_runiverse())
-vdat_to_folder <- function(vdata_file,
-                           outdir = getwd(),
-                           time_corrected = FALSE) {
-  # fls <- list.files('c:/users/darpa2/analysis/chesapeake-backbone/embargo/raw/20230912', pattern = 'VR2AR_\\d.*vrl', full.names = T)
-
-  # Check that only 1 file has been passed
-  if (length(vdata_file) > 1) {
-    error_too_many_files()
-  }
-
-
-  # Build arguments ----
-  ## Mandatory arguments
-  vdat_args <- c(
-    "convert",
-    "--format=csv.fathom.split",
-    paste0("--output=", outdir),
-    vdata_file
-  )
-
-
-  ## Optional arguments
-  ### Time correction
-  if (time_corrected == TRUE) {
-    vdat_args <- c(
-      vdat_args,
-      "--timec=default"
-    )
-  }
-
-
-  # Execute call to vdat.exe ----
-  shell_out <- vdat_call(
-    what = vdat_args
-  )
-
-
-  # Update user ----
-  if (shell_out$status == 1) {
-    error_convert(shell_out, vdata_file)
-  } else {
-    cli::cli_bullets(
-      c(
-        "v" = "File converted:",
-        " " = "{vdata_file}\n",
-        "i" = "Directory saved in:\n",
-        " " = "{outdir}"
-      )
-    )
-  }
+#' @examplesIf all(skip_example_on_ci(), skip_example_on_runiverse())
+vdat_to_folder <- function(...) {
+  vdat_to_csv(..., folder = TRUE)
 }
