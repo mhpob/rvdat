42b00a428e9cd860a5c936e1dde115b6d2667398
Mike O'Brien <mikeob9@gmail.com>
2024-01-09 16:32:08 -0500

    parse HR files

--- R/vdat_inspect.R
+++ R/vdat_inspect.R
@@ -17,19 +17,82 @@ vdat_inspect <- function(vdata_file, ...) {
     error_convert(shell_out, vdata_file)
   }
 
-  # Coerce to data frame
-  out <- shell_out$stdout |>
-    rawToChar() |>
-    strsplit("\\\r\\\n")
-  out <- out[[1]] # break pipe to pass R CMD on old Ubuntu
+  # Convert output into data frame
+  ## Parse the output
+  metadata <- shell_out$stdout |>
+    sys::as_text()
 
-  out <- out[sapply(out, function(.) grepl(":", .))] |>
+  ## Remove section dividers
+  metadata <- metadata[sapply(metadata, function(.) !grepl("=|^$", .))]
+
+  ## Parse section headers
+  ### Find section header indices (rows with >= 12 spaces)
+  section_headers <- metadata |>
+    grepl('\\s{12,}', x = _) |>
+    which()
+
+  ### Repeat those indices however many times to match the number of rows
+  ###   in the section
+  section_headers <- metadata[
+    section_headers[
+      # Repeat the headers however many times
+      findInterval(seq_along(metadata), vec = section_headers)[
+        # But not the headers, themselves
+        -section_headers
+        ]
+      ]
+    ] |>
+    # Remove the spaces
+    gsub('\\s', '', x = _)
+
+  ## Parse the metadata
+  ### Drop section headers by selecting rows that have a ":" or start with
+  ###   11 spaces then a character
+  metadata <- metadata[grepl(":|^\\s{11}[[:alnum:]]", metadata)]
+
+  ### Split according to colons followed by multiple spaces.
+  k <- metadata |>
     strsplit(":\\s+") |>
+
+    ### Variables with multiple entries have those entries indented and split
+    ###   into different lines. Split these into two columns according to a
+    ###   space that precedes an alphanumeric character
+    lapply(function(.){
+      unlist(
+        strsplit(., '\\s{2,}(?=[[:alpha:]])', perl = T)
+      )
+    }) |>
+
+    ### Now that every section has two entries, bind them together
     do.call(rbind, args = _) |>
     data.frame()
 
-  names(out) <- c("variable", "value")
+  ### Fill in blanks using last observation carried forward
+  ###   Function taken from:
+  ###   https://stackoverflow.com/questions/10554741/fill-in-data-frame-with-values-from-rows-above
+
+  locf <- function(x, blank = is.na) {
+    # Find the values
+    if (is.function(blank)) {
+      isnotblank <- !blank(x)
+    } else {
+      isnotblank <- x != blank
+    }
+    # Fill down
+    x[which(isnotblank)][cumsum(isnotblank)]
+  }
+
+  metadata$X1 <- locf(metadata$X1, blank = '')
+
+  ### Add back section headers
+  metadata$section <- section_headers
+
+  ### Remove redundant variables
+  metadata <- metadata[metadata$X1 != metadata$X2,]
+
+  ### Rename
+  names(metadata) <- c("variable", "value", "section")
 
 
-  invisible(out)
+  invisible(metadata)
 }
