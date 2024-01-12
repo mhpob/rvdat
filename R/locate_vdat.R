7a70e5e12a4dd74ab6e4b8060e6a2aad2b34118c
benjaminhlina <benjaminhlina@users.noreply.github.com>
2024-01-12 14:17:35 +0000

    Style code (GHA)

--- R/locate_vdat.R
+++ R/locate_vdat.R
@@ -7,16 +7,17 @@
 
 #' @export
 locate_vdat <- function(sys = NULL, version = NULL) {
-
-  if(is.null(version)) {
+  if (is.null(version)) {
     version <- "vdat-7.0.0-20231107"
   }
-  if(is.null(sys)) {
+  if (is.null(sys)) {
     sys <- "Linux-x86_64"
   }
   vdat_name <- "vdat.exe"
   fs::path_abs(path = paste("../vdat-working-group-master/releases", version,
-                            sys, vdat_name, sep = "/"))
+    sys, vdat_name,
+    sep = "/"
+  ))
   # list.files(paste("../vdat-working-group-master/releases", version, sys, sep = "/"),
   #            recursive = TRUE,
   #            full.names = TRUE,
@@ -26,11 +27,14 @@ locate_vdat <- function(sys = NULL, version = NULL) {
 }
 list.dirs()
 fs::path_abs(path = paste("../vdat-working-group-master/releases", version,
-                          sys, sep = "/"))
+  sys,
+  sep = "/"
+))
 ls()
 fs::dir_ls(
   path = c("C:/", "E:/"),
-           type = "directory", , recurse = TRUE)
+  type = "directory", , recurse = TRUE
+)
 
 
 list.dirs(path = "../vdat-working-group-master/releases")
