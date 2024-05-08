3de8791a05b2e69c1b07c66a504affc53bb6f237
Mike O'Brien <mikeob9@gmail.com>
2024-05-08 15:33:17 -0400

    update glatos scrape for new GH structure

--- tests/testthat/setup-testfiles.R
+++ tests/testthat/setup-testfiles.R
@@ -11,17 +11,20 @@ dir.create(
 # Use test files from glatos' dev branch
 ## Parse /dev/inst/extdata/detection_files_raw directory, pick vdat files,
 ##  and make their download URLs
-testfiles <- "https://github.com/ocean-tracking-network/glatos/tree/dev/inst/extdata/detection_files_raw" |>
+testfiles <-
+  "https://github.com/ocean-tracking-network/glatos/tree/dev/inst/extdata/detection_files_raw" |>
   readLines(warn = FALSE) |>
-  strsplit("path")
-testfiles <- testfiles[[1]] |>
   grep("detection_files_raw.*\\.v", x = _, value = T) |>
+  strsplit("path")
+
+testfiles <- testfiles[[1]] |>
   strsplit('[:,"]') |>
   lapply(function(.) .[grepl("inst.*\\.v", .)]) |>
   unlist()
+
 testfiles <- testfiles[!duplicated(gsub("[_ ].*\\.", "", basename(testfiles)))]
 testfiles <- testfiles |>
-  file.path("https://github.com/ocean-tracking-network/glatos/raw/dev/",
+  file.path("https://github.com/ocean-tracking-network/glatos/raw/dev",
     ... = _
   ) |>
   URLencode()
